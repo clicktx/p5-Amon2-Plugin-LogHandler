cde8ec0c21c914d50b5b5bf0ca434e9c64aba79c
clicktx <clicktx@gmail.com>
2016-03-29 17:43:50 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::Plugin::LogHandler - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::Plugin::LogHandler;
+
+# DESCRIPTION
+
+Amon2::Plugin::LogHandler is ...
+
+# LICENSE
+
+Copyright (C) clicktx.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+clicktx <clicktx@gmail.com>
---
+++ lib/Amon2/Plugin/LogHandler.pm
@@ -0,0 +1,39 @@
+package Amon2::Plugin::LogHandler;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::Plugin::LogHandler - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::Plugin::LogHandler;
+
+=head1 DESCRIPTION
+
+Amon2::Plugin::LogHandler is ...
+
+=head1 LICENSE
+
+Copyright (C) clicktx.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+clicktx E<lt>clicktx@gmail.comE<gt>
+
+=cut
+
